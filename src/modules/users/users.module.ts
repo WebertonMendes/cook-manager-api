26097950c672ac4f2f318ba6604bf4959c4f31a0
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2023-11-13 16:36:36 -0300

    controller and use case for find-all-users added in the module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,12 +4,18 @@ import { DatabaseModule } from '@/infra/database/database.module';
 import { CryptographyModule } from '@/infra/helpers/cryptography/cryptography.module';
 import { CreateUsersController } from './usecases/create-user/create-user.controller';
 import { CreateUserUseCase } from './usecases/create-user/create-user.usecase';
+import { FindAllUsersController } from './usecases/find-all-users/find-all-users.controller';
+import { FindAllUsersUseCase } from './usecases/find-all-users/find-all-users.usecase';
 import { FindUserByIdController } from './usecases/find-user-by-id/find-user-by-id.controller';
 import { FindUserByIdUseCase } from './usecases/find-user-by-id/find-user-by-id.usecase';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateUsersController, FindUserByIdController],
-  providers: [CreateUserUseCase, FindUserByIdUseCase],
+  controllers: [
+    CreateUsersController,
+    FindUserByIdController,
+    FindAllUsersController,
+  ],
+  providers: [CreateUserUseCase, FindUserByIdUseCase, FindAllUsersUseCase],
 })
 export class UsersModule {}
