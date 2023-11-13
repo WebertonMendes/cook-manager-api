fef4c007f0cbb3fd0c21c8028405dd56019f4e39
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2023-11-13 16:59:51 -0300

    controller and use case for delete-user added in the module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,6 +4,8 @@ import { DatabaseModule } from '@/infra/database/database.module';
 import { CryptographyModule } from '@/infra/helpers/cryptography/cryptography.module';
 import { CreateUsersController } from './usecases/create-user/create-user.controller';
 import { CreateUserUseCase } from './usecases/create-user/create-user.usecase';
+import { DeleteUserController } from './usecases/delete-user/delete-user.controller';
+import { DeleteUserUseCase } from './usecases/delete-user/delete-user.usecase';
 import { FindAllUsersController } from './usecases/find-all-users/find-all-users.controller';
 import { FindAllUsersUseCase } from './usecases/find-all-users/find-all-users.usecase';
 import { FindUserByIdController } from './usecases/find-user-by-id/find-user-by-id.controller';
@@ -15,7 +17,13 @@ import { FindUserByIdUseCase } from './usecases/find-user-by-id/find-user-by-id.
     CreateUsersController,
     FindUserByIdController,
     FindAllUsersController,
+    DeleteUserController,
   ],
-  providers: [CreateUserUseCase, FindUserByIdUseCase, FindAllUsersUseCase],
+  providers: [
+    CreateUserUseCase,
+    FindUserByIdUseCase,
+    FindAllUsersUseCase,
+    DeleteUserUseCase,
+  ],
 })
 export class UsersModule {}
