6f96026915df406e2908ebdd1f89344be5f6a91e
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2023-11-13 16:22:24 -0300

    controller and use case for find-user-by-id added in the module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,10 +4,12 @@ import { DatabaseModule } from '@/infra/database/database.module';
 import { CryptographyModule } from '@/infra/helpers/cryptography/cryptography.module';
 import { CreateUsersController } from './usecases/create-user/create-user.controller';
 import { CreateUserUseCase } from './usecases/create-user/create-user.usecase';
+import { FindUserByIdController } from './usecases/find-user-by-id/find-user-by-id.controller';
+import { FindUserByIdUseCase } from './usecases/find-user-by-id/find-user-by-id.usecase';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateUsersController],
-  providers: [CreateUserUseCase],
+  controllers: [CreateUsersController, FindUserByIdController],
+  providers: [CreateUserUseCase, FindUserByIdUseCase],
 })
 export class UsersModule {}
