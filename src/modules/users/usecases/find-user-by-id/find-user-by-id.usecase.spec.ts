ec14030e49f787f28d564e278ae97a40b9fe27fa
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2023-11-13 16:24:05 -0300

    add unit test for find-user-by-id

---
+++ src/modules/users/usecases/find-user-by-id/find-user-by-id.usecase.spec.ts
@@ -0,0 +1,46 @@
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-user.repository';
+import { FindUserByIdUseCase } from './find-user-by-id.usecase';
+import { UserNotFoundException } from '../../exceptions/user-not-found-exception';
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let findUserById: FindUserByIdUseCase;
+
+describe('Create User', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    findUserById = new FindUserByIdUseCase(inMemoryUsersRepository);
+  });
+
+  it('should be able to find user by id', async () => {
+    const newUser = {
+      name: 'Webs System',
+      username: 'Webs.System',
+      avatarUrl: 'https://github.com/WebertonMendes.png',
+      password: 'password123',
+    };
+
+    await inMemoryUsersRepository.createUser(newUser);
+
+    const savedUser = await inMemoryUsersRepository.findByUsername(
+      newUser.username,
+    );
+
+    const user = await findUserById.execute(savedUser.id);
+
+    expect(savedUser.id).toEqual(user.id);
+    expect(savedUser.name).toEqual(user.name);
+    expect(savedUser.username).toEqual(user.username);
+  });
+
+  it('should be able to throw a UserNotFoundException if the user id not found.', async () => {
+    const fakeUserId = 'fake-userId';
+
+    try {
+      await findUserById.execute(fakeUserId);
+    } catch (error) {
+      expect(() => {
+        throw new UserNotFoundException(fakeUserId);
+      }).toThrow(UserNotFoundException);
+    }
+  });
+});
