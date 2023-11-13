ca7b1e768e9fcaf63bd8076ac22cd7bdf7e693c7
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2023-11-13 16:27:21 -0300

    add unit test for find-user-by-id

---
+++ test/factories/make-users.ts
@@ -0,0 +1,29 @@
+import { faker } from '@faker-js/faker';
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { UserEntity } from '@/modules/users/entities/user.entity';
+
+export function makeUser(override: Partial<UserEntity> = {}) {
+  return new UserEntity({
+    name: faker.person.fullName(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...override,
+  });
+}
+
+@Injectable()
+export class UserFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaUser(data: Partial<UserEntity> = {}): Promise<UserEntity> {
+    const user = makeUser(data);
+
+    await this.prisma.user.create({
+      data: user,
+    });
+
+    return user;
+  }
+}
