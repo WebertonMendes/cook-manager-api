6e18e98f63a47167f78186196943f592df3a4d37
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2023-12-27 01:30:37 -0300

    create unit tests with create category use case

---
+++ src/modules/categories/usecases/create-category/create-category.usecase.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryCategoriesRepository } from 'test/repositories/in-memory-category.repository';
+import { CategoryAlreadyExistsException } from '../../exceptions/category-already-exists-exception';
+import { CreateCategoryUseCase } from './create-category.usecase';
+
+let inMemoryCategoriesRepository: InMemoryCategoriesRepository;
+
+let createCategory: CreateCategoryUseCase;
+
+describe('Create User', () => {
+  beforeEach(() => {
+    inMemoryCategoriesRepository = new InMemoryCategoriesRepository();
+    createCategory = new CreateCategoryUseCase(inMemoryCategoriesRepository);
+  });
+
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'Drinks',
+    };
+
+    await createCategory.execute(category);
+
+    const savedCategory = inMemoryCategoriesRepository.items[0];
+
+    expect(savedCategory.name).toEqual(category.name);
+    expect(savedCategory).toHaveProperty('id');
+  });
+
+  it('should be able to throw a CategoryAlreadyExistsException if the category name already exists.', async () => {
+    const category = {
+      name: 'Salads',
+    };
+
+    try {
+      await createCategory.execute(category);
+      await createCategory.execute(category);
+    } catch (error) {
+      expect(() => {
+        throw new CategoryAlreadyExistsException(category.name);
+      }).toThrow(CategoryAlreadyExistsException);
+    }
+  });
+});
