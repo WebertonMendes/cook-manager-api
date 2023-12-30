97121c62e1d64471cbb8d3af9cb492c38c9fe5df
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2023-12-30 00:17:25 -0300

    create e2e tests with create product use case

---
+++ src/modules/products/usecases/create-product/create-product.controller.e2e-spec.ts
@@ -0,0 +1,77 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+import { AppModule } from '@/infra/app.module';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+
+describe('Create product (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[POST] /products', async () => {
+    await request(app.getHttpServer()).post('/categories').send({
+      name: 'Drinks',
+    });
+
+    const category = await prisma.category.findUnique({
+      where: {
+        name: 'Drinks',
+      },
+    });
+
+    const productData = {
+      name: 'Product Name',
+      description: 'Product Description',
+      price: 10.99,
+      imageUrl: 'https://mysite.com/images/product001.png',
+      categoryId: category.id,
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/products')
+      .send(productData);
+
+    const productOnDatabase = await prisma.product.findFirst({
+      where: {
+        name: productData.name,
+      },
+    });
+
+    expect(response.statusCode).toBe(HttpStatus.CREATED);
+    expect(productOnDatabase).toBeTruthy();
+  });
+
+  test('[POST] /products throw CategoryNotFoundException', async () => {
+    const productData = {
+      name: 'Product Name',
+      description: 'Product Description',
+      price: 10.99,
+      imageUrl: 'https://mysite.com/images/product001.png',
+      categoryId: 'fake-category-id',
+    };
+
+    await request(app.getHttpServer()).post('/products').send(productData);
+
+    const response = await request(app.getHttpServer())
+      .post('/products')
+      .send(productData);
+
+    expect(response.statusCode).toBe(HttpStatus.NOT_FOUND);
+    expect(response.body.statusCode).toEqual(HttpStatus.NOT_FOUND);
+    expect(response.body.message).toEqual(
+      `Category '${productData.categoryId}' not found.`,
+    );
+  });
+});
