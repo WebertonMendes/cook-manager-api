cecb705717a861a0f2f7ae135c0e73c567a65830
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2024-02-07 16:21:50 -0300

    add modules order and order-items

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 
 import { CategoriesRepository } from '@/modules/categories/repositories/categories.repository';
+import { OrderItemsRepository } from '@/modules/items/repositories/order-items.repository';
+import { OrdersRepository } from '@/modules/orders/repositories/orders.repository';
+import { ProductsRepository } from '@/modules/products/repositories/products.repository';
 import { UsersRepository } from '@/modules/users/repositories/users.repository';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaCategoriesRepository } from './prisma/repositories/prisma-categories.repository';
-import { PrismaUsersRepository } from './prisma/repositories/prisma-users.repository';
-import { ProductsRepository } from '@/modules/products/repositories/products.repository';
+import { PrismaOrderItemsRepository } from './prisma/repositories/prisma-order-items.repository';
+import { PrismaOrdersRepository } from './prisma/repositories/prisma-orders.repository';
 import { PrismaProductsRepository } from './prisma/repositories/prisma-products.repository';
+import { PrismaUsersRepository } from './prisma/repositories/prisma-users.repository';
 
 @Module({
   providers: [
@@ -14,6 +18,14 @@ import { PrismaProductsRepository } from './prisma/repositories/prisma-products.
     {
       provide: CategoriesRepository,
       useClass: PrismaCategoriesRepository,
+    },
+    {
+      provide: OrdersRepository,
+      useClass: PrismaOrdersRepository,
+    },
+    {
+      provide: OrderItemsRepository,
+      useClass: PrismaOrderItemsRepository,
     },
     {
       provide: ProductsRepository,
@@ -26,6 +38,8 @@ import { PrismaProductsRepository } from './prisma/repositories/prisma-products.
   ],
   exports: [
     CategoriesRepository,
+    OrdersRepository,
+    OrderItemsRepository,
     PrismaService,
     ProductsRepository,
     UsersRepository,
--- src/modules/http.module.ts
+++ src/modules/http.module.ts
@@ -3,8 +3,16 @@ import { Module } from '@nestjs/common';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { OrderItemsModule } from './items/order-items.module';
 
 @Module({
-  imports: [CategoriesModule, ProductsModule, UsersModule],
+  imports: [
+    CategoriesModule,
+    ProductsModule,
+    UsersModule,
+    OrdersModule,
+    OrderItemsModule,
+  ],
 })
 export class HttpModule {}
