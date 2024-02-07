3a88b9dd8ed63440a3a03a4b4b666d91a6e4d048
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2024-02-07 16:11:57 -0300

    add orders module

---
+++ src/modules/orders/orders.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+
+import { DatabaseModule } from '@/infra/database/database.module';
+import { CreateOrdersController } from './usecases/create-order/create-order.controller';
+import { CreateOrderUseCase } from './usecases/create-order/create-order.usecase';
+import { DeleteOrderController } from './usecases/delete-order/delete-order.controller';
+import { DeleteOrderUseCase } from './usecases/delete-order/delete-order.usecase';
+import { FindAllOrdersController } from './usecases/find-all-orders/find-all-orders.controller';
+import { FindAllOrdersUseCase } from './usecases/find-all-orders/find-all-orders.usecase';
+import { FindOrderByIdController } from './usecases/find-order-by-id/find-order-by-id.controller';
+import { FindOrderByIdUseCase } from './usecases/find-order-by-id/find-order-by-id.usecase';
+import { UpdateOrderController } from './usecases/update-order/update-order.controller';
+import { UpdateOrderUseCase } from './usecases/update-order/update-order.usecase';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [
+    CreateOrdersController,
+    FindOrderByIdController,
+    FindAllOrdersController,
+    UpdateOrderController,
+    DeleteOrderController,
+  ],
+  providers: [
+    CreateOrderUseCase,
+    FindOrderByIdUseCase,
+    FindAllOrdersUseCase,
+    UpdateOrderUseCase,
+    DeleteOrderUseCase,
+  ],
+})
+export class OrdersModule {}
