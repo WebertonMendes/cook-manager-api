3e1ab6f1025ecba6d43900cba8c1b8b54be9865e
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2024-02-08 01:29:40 -0300

    create e2e tests with delete order use case

---
+++ src/modules/orders/usecases/delete-order/delete-order.controller.e2e-spec.ts
@@ -0,0 +1,90 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+import { beforeAll, describe, expect, test } from 'vitest';
+
+import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { OrderItemFactory } from 'test/factories/make-order-items';
+import { OrderFactory } from 'test/factories/make-orders';
+
+describe('Delete order by ID (E2E)', () => {
+  let app: INestApplication;
+  let orderFactory: OrderFactory;
+  let orderItemFactory: OrderItemFactory;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [OrderFactory, OrderItemFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    orderFactory = moduleRef.get(OrderFactory);
+    orderItemFactory = moduleRef.get(OrderItemFactory);
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[DELETE] /orders/:id', async () => {
+    const order = await orderFactory.makePrismaOrder();
+
+    const response = await request(app.getHttpServer())
+      .delete(`/orders/${order.id}`)
+      .send();
+
+    const orderOnDatabase = await prisma.order.findUnique({
+      where: { id: order.id },
+    });
+
+    expect(response.statusCode).toBe(HttpStatus.OK);
+    expect(orderOnDatabase).toBeNull();
+  });
+
+  test('[DELETE] /orders/:id throw OrderNotFoundException', async () => {
+    const orderId = 'fake-orderId';
+
+    const response = await request(app.getHttpServer())
+      .delete(`/orders/${orderId}`)
+      .send();
+
+    expect(response.statusCode).toBe(HttpStatus.NOT_FOUND);
+    expect(response.body.statusCode).toEqual(HttpStatus.NOT_FOUND);
+    expect(response.body.message).toEqual(`Order '${orderId}' not found.`);
+  });
+
+  test('[DELETE] /orders/:id throw OrderInProgressException', async () => {
+    const orderItem = await orderItemFactory.makePrismaOrderItem();
+
+    const response = await request(app.getHttpServer())
+      .delete(`/orders/${orderItem.orderId}`)
+      .send();
+
+    expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+    expect(response.body.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+    expect(response.body.message).toEqual(
+      `Order '${orderItem.orderId}' cannot be removed as it has already in progress.`,
+    );
+  });
+
+  test('[DELETE] /orders/:id throw OrderIsFinishedException', async () => {
+    const order = await orderFactory.makePrismaOrder();
+
+    await request(app.getHttpServer()).patch(`/orders/${order.id}`).send({
+      isFinished: true,
+    });
+
+    const response = await request(app.getHttpServer())
+      .delete(`/orders/${order.id}`)
+      .send();
+
+    expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+    expect(response.body.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+    expect(response.body.message).toEqual(
+      `Order '${order.id}' cannot be removed as it has already been finished.`,
+    );
+  });
+});
